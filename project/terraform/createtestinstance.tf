e47866d2bfb758130325de581245718efb7053c9
ArtsiomFortunatov <artsiom@admin.com>
2021-10-30 22:55:48 +0300

    change  infra

--- project/terraform/createtestinstance.tf
+++ project/terraform/createtestinstance.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "testInstance" {
  
   ami           = "${var.instance_ami}"
   instance_type = "${var.instance_type}"
-  subnet_id = "${aws_subnet.subnet_public.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
+  #subnet_id = "${aws_subnet.subnet_public.id}"
+  #vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
   key_name = "${aws_key_pair.ec2key.key_name}"
   tags = {
   Environment = "${var.environment_tag}"
