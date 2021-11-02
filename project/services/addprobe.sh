7c5d94e746c4b5aa7b152ec88b265eed15a164a3
ArtsiomFortunatov <artsiom@admin.com>
2021-11-02 22:44:52 +0300

    change sh

--- project/services/addprobe.sh
+++ project/services/addprobe.sh
@@ -2,14 +2,14 @@
 
 cat <<EOT >> pod1.yaml
  
- readinessProbe:
-   tcpSocket:
-     port: 80
-   initialDelaySeconds: 5
-   periodSeconds: 10
- livenessProbe:
-   tcpSocket:
-     port: 80
-   initialDelaySeconds: 15
-   periodSeconds: 20
+   readinessProbe:
+     tcpSocket:
+       port: 80
+     initialDelaySeconds: 5
+     periodSeconds: 10
+   livenessProbe:
+     tcpSocket:
+       port: 80
+     initialDelaySeconds: 15
+     periodSeconds: 20
 EOT
