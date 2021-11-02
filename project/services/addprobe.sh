f65983fa41a6162ccdb91987cc116bcf7973a851
ArtsiomFortunatov <artsiom@admin.com>
2021-11-02 22:39:57 +0300

    build

--- project/services/addprobe.sh
+++ project/services/addprobe.sh
@@ -3,12 +3,12 @@
 cat <<EOT >> pod1.yaml
  readinessProbe:
    tcpSocket:
-     port: 8080
+     port: 80
    initialDelaySeconds: 5
    periodSeconds: 10
  livenessProbe:
    tcpSocket:
-     port: 8080
+     port: 80
    initialDelaySeconds: 15
    periodSeconds: 20
 EOT
